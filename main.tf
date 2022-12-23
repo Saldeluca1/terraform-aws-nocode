d4acc8e6723cad673b75bea6e5f5f86a9915f483
peada04 <dpeacock@hashicorp.com>
2022-12-23 09:32:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "aws_instance" "web" {
     # The AMI ID must refer to an AMI that contains an operating system
     # for the `x86_64` architecture.
     precondition {
-      condition     = data.aws_ami.example.architecture == "x86_64"
+      condition     = self.aws_ami.example.architecture == "x86_64"
       error_message = "The selected AMI must be for the x86_64 architecture."
     }
 
