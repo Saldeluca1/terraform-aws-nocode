0b43bcf04fa549d06c31a2fb2e50f43a6e69c6a4
peada04 <dpeacock@hashicorp.com>
2024-07-15 16:25:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = var.ec2-size
   count         = 1
 
   tags = {
