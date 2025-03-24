32a1ff08915e97e028bd611286672da72f001531
Saldeluca1 <Sal.Deluca@hashicorp.com>
2025-03-24 16:14:37 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,10 +2,3 @@ output "instance_public_ip" {
   value = aws_instance.web.*.public_ip
 }
 
-output "vault-aws-lease-id" {
-  value = data.vault_aws_access_credentials.aws_creds.lease_id
-}
-
-output "vault-aws-lease-duration" {
-  value = data.vault_aws_access_credentials.aws_creds.lease_duration
-}
